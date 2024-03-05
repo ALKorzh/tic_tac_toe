a4e0ef34ec95cff29a1536bd73e8007978691ceb
ALKorzh <alexkorzhwork@gmail.com>
2024-03-05 16:52:33 +0300

    add draw game logic, output div

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const button = document.querySelector("#set-field-size")
 const input = document.querySelector("#input-field-size")
 const matrixEl = document.querySelector("#table")
+const output = document.querySelector(".output")
 
 let matrix = []
 let fieldSize
 const playersSymbols = ["O", "X"]
-const playersNumbersOfSymbols = [-1, 1]
+const playerSymbolValues = [-1, 1]
 let moveCounter = 0
 
 function createMatrix(fieldSize) {
@@ -29,16 +30,16 @@ function render() {
   })
 
   document.querySelectorAll(".cell").forEach((cell) => {
-    cell.addEventListener("click", fillCell)
+    cell.addEventListener("click", updateCellAndMatrix)
   })
 }
 
-function fillCell() {
+function updateCellAndMatrix() {
   moveCounter++
   this.innerHTML = `<p>${
     playersSymbols[moveCounter % playersSymbols.length]
   }</p>`
-  this.removeEventListener("click", fillCell)
+  this.removeEventListener("click", updateCellAndMatrix)
 
   let numberOfCell = this.id.match(/\d+/g)[0]
   let placeCounter = 0
@@ -46,19 +47,28 @@ function fillCell() {
     row.forEach((el, i) => {
       placeCounter++
       if (placeCounter == numberOfCell) {
-        row[i] =
-          playersNumbersOfSymbols[moveCounter % playersNumbersOfSymbols.length]
+        row[i] = playerSymbolValues[moveCounter % playerSymbolValues.length]
       }
     })
   })
-  console.log(isWinField(matrix))
+  console.log(isWinningBoard(matrix))
   console.log(matrix)
-  if (isWinField(matrix)) {
+
+  if (isWinningBoard(matrix)) {
     document.querySelectorAll(".cell").forEach((cell) => {
-      cell.removeEventListener("click", fillCell)
+      cell.removeEventListener("click", updateCellAndMatrix)
     })
+    showWinMessage()
+    return null
+  } else {
+    if (isFilledField(matrix)) {
+      showDrawMessage()
+      return null
+    }
   }
+  respondOnEvent()
 }
+
 function transpose(matrix) {
   return matrix[0].map((col, i) => {
     return matrix.map((row) => {
@@ -66,11 +76,13 @@ function transpose(matrix) {
     })
   })
 }
+
 function findMainSecondaryDiagonals(matrix) {
   const mainDiagonal = matrix.map((row, i) => row[i])
   const secondaryDiagonal = matrix.map((row, i) => row[row.length - i - 1])
   return (diagonals = [mainDiagonal, secondaryDiagonal])
 }
+
 function isWinRow(matrix, fieldSize) {
   let flag = false
   matrix.forEach((row) => {
@@ -81,25 +93,58 @@ function isWinRow(matrix, fieldSize) {
     console.log(rowSum)
     if (
       rowSum ==
-      playersNumbersOfSymbols[moveCounter % playersNumbersOfSymbols.length] *
-        fieldSize
+      playerSymbolValues[moveCounter % playerSymbolValues.length] * fieldSize
     ) {
       flag = true
     }
   })
   return flag
 }
-function isWinField() {
+
+function isWinningBoard() {
   return (
     isWinRow(matrix, fieldSize) ||
     isWinRow(transpose(matrix), fieldSize) ||
     isWinRow(findMainSecondaryDiagonals(matrix), fieldSize)
   )
 }
+function respondOnEvent() {
+  output.innerHTML += "<div class='message'></div>"
+  let messages = document.querySelectorAll(".message")
+  messages[messages.length - 1].innerHTML += `<p>Ход ${
+    moveCounter + 1
+  } - ходит ${playersSymbols[(moveCounter + 1) % playersSymbols.length]}</p>`
+  output.style.height = `${document.querySelector("#table").offsetHeight}px`
+  console.log(document.querySelector("#table").offsetHeight)
+}
+function showWinMessage() {
+  let messages = document.querySelectorAll(".message")
+  messages[messages.length - 1].innerHTML += `<p>Победил ${
+    playersSymbols[moveCounter % playersSymbols.length]
+  }. На ${moveCounter} ходу</p>`
+  messages[messages.length - 1].lastChild.style.color = "green"
+}
+function showDrawMessage() {
+  let messages = document.querySelectorAll(".message")
+  messages[
+    messages.length - 1
+  ].innerHTML += `<p>Ничья. На ${moveCounter} ходу</p>`
+}
+function isFilledField(matrix) {
+  let flag = false
+  matrix.forEach((row) => {
+    if (!row.includes(0)) {
+      flag = true
+    }
+  })
+  return flag
+}
+
 button.addEventListener("click", () => {
   fieldSize = +input.value
   console.log(fieldSize)
   button.disabled = true
   input.disabled = true
   render()
+  respondOnEvent()
 })
